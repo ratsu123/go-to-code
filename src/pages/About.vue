8e5d4e21da7298421ecdb788b0023a63cdebf823
Ratmir <shikirmik2002@gmail.com>
2022-08-08 15:15:44 +0600

    gtc

---
+++ src/pages/About.vue
@@ -0,0 +1,14 @@
+<template>
+  <Layout>
+    <h1 class="text-white">About us</h1>
+    <p class="text-red">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error doloremque omnis animi, eligendi magni a voluptatum, vitae, consequuntur rerum illum odit fugit assumenda rem dolores inventore iste reprehenderit maxime! Iusto.</p>
+  </Layout>
+</template>
+
+<script>
+export default {
+  metaInfo: {
+    title: 'About us'
+  }
+}
+</script>
---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  content: ["./src/**/*.{html,js,vue}"],
+  purge: {
+    content: [
+      './src/**/*.vue',
+      './src/**/*.js',
+      './src/**/*.jsx',
+      './src/**/*.html',
+      './src/**/*.pug',
+      './src/**/*.md'
+    ]
+  },
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
